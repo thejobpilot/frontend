affc3a6ad5417e00cbfb32996d98644544bd1030
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-02-16 20:12:57 -0500

    Move branding to public dir

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -39,7 +39,7 @@ function ResponsiveAppBar() {
         <AppBar position="fixed">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <img src="/images/JobPilot.png" alt="Logo" style={{ width: '45px', height: '45px', marginRight: '22px'}}/>
+                    <img src="/images/pngb.png" alt="Logo" style={{ width: '45px', height: '45px', marginRight: '22px'}}/>
                     <Typography
                         variant="h6"
                         noWrap
