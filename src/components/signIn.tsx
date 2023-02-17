5aad892161a44ad2db8144e39046d40f3ee3cf74
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-02-16 22:15:30 -0500

    Setup auth0 login page

--- src/components/signIn.tsx
+++ src/components/signIn.tsx
@@ -18,7 +18,7 @@ function Copyright(props: any) {
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
             {'Copyright © '}
             <Link color="inherit" href="https://mui.com/">
-                Your Website
+                JobPilot
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
@@ -39,77 +39,51 @@ export default function SignIn() {
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 22,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="/forgotpassword" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item xs>
-                                <Link href="/recruiter/signup" variant="body2">
-                                    Recruiter Sign Up
-                                </Link>
-                            </Grid>
-                            <Grid item xs>
-                                <Link href="/applicant/signup" variant="body2">
-                                    Applicant Sign Up
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 22,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Welcome motherFuckers
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                href="/api/auth/login"
+              >
+                Sign In
+              </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                href="/api/auth/logout"
+              >
+                Sign Out
+              </Button>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
     );
 }
--- src/pages/dash.tsx
+++ src/pages/dash.tsx
@@ -1,15 +1,44 @@
-import { Inter } from "@next/font/google";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
-import NavBar from "@/components/navBar";
-import SignIn from "@/components/signIn";
+import { withAuth0, WithAuth0Props } from "@auth0/auth0-react";
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 import ResponsiveAppBar from "@/components/navBar";
+import React, {Component} from 'react'
 
-const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Profile() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
-    <>
-      <ResponsiveAppBar></ResponsiveAppBar>
-    </>
+    user && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
   );
 }
+
+class Dashboard extends Component<WithAuth0Props> {
+  render() {
+      const { user, error, isLoading } = this.props.auth0;
+
+      if (isLoading) return <div>Loading...</div>;
+      if (error) return <div>{error.message}</div>;
+
+      return (
+        user && (
+          <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        )
+      );
+  }
+}
+
+
