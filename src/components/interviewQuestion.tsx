abb44c19291dd57134b57af29e7428f4df269562
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-02-19 20:09:55 -0500

    interviewQuestion: figured out top-padding

--- src/components/interviewQuestion.tsx
+++ src/components/interviewQuestion.tsx
@@ -2,14 +2,24 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { spacing } from '@mui/system';
+
+
 
 export default function QuestionContainter() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="md">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      <Box sx={{ bgcolor: '#cfe8fc', height: '35vh', mt: 10 }}> 
+        <Typography variant="h1">Lizard</Typography>
+      </Box> 
+        
       </Container>
     </React.Fragment>
+    
+
+    
   );
 }
