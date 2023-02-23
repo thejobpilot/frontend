2460ae6c8c554d717695c8d10c639b01706010f6
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-02-23 16:26:39 -0500

    Redirect dash to app or recruiter

--- src/pages/recruiter/dash/index.tsx
+++ src/pages/recruiter/dash/index.tsx
@@ -3,7 +3,7 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 
 import ResponsiveAppBar from "@/components/navBar";
 import Button from "@mui/material/Button";
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default function Profile() {
@@ -12,12 +12,27 @@ export default function Profile() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
-  useEffect(() => {
-    user &&
-    window.location.replace("/applicant/dash");
-  }, []);
-
-  return null;
+  return (
+    user && (
+      <div>
+        {user.picture && user.name && (
+          <img src={user.picture} alt={user.name} />
+        )}
+        <h1>Recruiter/Dash</h1>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          href="/api/auth/logout"
+        >
+          Sign Out
+        </Button>
+      </div>
+    )
+  );
 }
 
 export const getServerSideProps = withPageAuthRequired();
