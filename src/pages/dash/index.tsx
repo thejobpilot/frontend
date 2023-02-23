12bc850af65096a99122290c30b94fabc73d0a52
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-02-23 16:30:37 -0500

    Fix eslint violations

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -2,20 +2,17 @@ import { withAuth0, WithAuth0Props } from "@auth0/auth0-react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
 import ResponsiveAppBar from "@/components/navBar";
-import Button from "@mui/material/Button";
 import React, { useEffect } from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 export default function Profile() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
   useEffect(() => {
-    user &&
     window.location.replace("/applicant/dash");
   }, []);
+
+  const { error, isLoading } = useUser();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
   return null;
 }
