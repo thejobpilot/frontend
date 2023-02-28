6482e734880f1eecbea2a7a63e0c233d7cf834f8
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-02-28 12:09:32 -0500

    Reroute user to correct dashboard on login

--- src/pages/dash/index.tsx
+++ src/pages/dash/index.tsx
@@ -1,13 +1,13 @@
-import { withAuth0, WithAuth0Props } from "@auth0/auth0-react";
 import { useUser } from "@auth0/nextjs-auth0/client";
-
-import ResponsiveAppBar from "@/components/navBar";
 import React, { useEffect } from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useRouter } from "next/router";
 
 export default function Profile() {
+  const router = useRouter();
+
   useEffect(() => {
-    window.location.replace("/applicant/dash");
+    router.push("/applicant/dash");
   }, []);
 
   const { error, isLoading } = useUser();
