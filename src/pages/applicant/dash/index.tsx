612efb1269032287aa7f5bd4c80ffa88905f2c31
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-02-28 13:31:36 -0500

    Add interview items to applicant dasboard

--- src/pages/applicant/dash/index.tsx
+++ src/pages/applicant/dash/index.tsx
@@ -1,10 +1,11 @@
-import { withAuth0, WithAuth0Props } from "@auth0/auth0-react";
 import { useUser } from "@auth0/nextjs-auth0/client";
-
 import ResponsiveAppBar from "@/components/navBar";
 import Button from "@mui/material/Button";
 import React, { Component } from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+import interviewTest from "../../../components/interviewTest";
+import interviewItem from "../../../components/interviewItem";
 
 export default function Profile() {
   const { user, error, isLoading } = useUser();
@@ -12,24 +13,32 @@ export default function Profile() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
+  const items = interviewTest();
   return (
     user && (
       <div>
-        {user.picture && user.name && (
-          <img src={user.picture} alt={user.name} />
-        )}
-        <h1>Applicant/Dash</h1>
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-          href="/api/auth/logout"
-        >
-          Sign Out
-        </Button>
+        <ResponsiveAppBar />
+
+        <div>
+          {user.picture && user.name && (
+            <img src={user.picture} alt={user.name} />
+          )}
+          <h1>Applicant/Dash</h1>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            href="/api/auth/logout"
+          >
+            Sign Out
+          </Button>
+          {
+            items.map(i => i)
+          }
+        </div>
       </div>
     )
   );
