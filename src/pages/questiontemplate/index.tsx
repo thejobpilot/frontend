10e6e0516612f5a74454d5dff1e839315ca00e4e
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-02-28 20:16:02 -0500

    interviewtemplate: gridded the page

--- src/pages/questiontemplate/index.tsx
+++ src/pages/questiontemplate/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {Inter} from '@next/font/google'
 import ResponsiveAppBar from "@/components/navBar";
 import Question from "@/components/interviewQuestion";
+import Grid from '@mui/material/Grid';
 const inter = Inter({subsets: ['latin']})
 
 export default function questiontemplate() {
@@ -10,11 +11,18 @@ export default function questiontemplate() {
   return (
     <>
     <ResponsiveAppBar></ResponsiveAppBar>
-
-    <Question num="1"></Question>
-    <Question num="2"></Question>
-    <Question num="3"></Question>
-
+    <Grid container 
+    spacing={1} 
+    rowSpacing={10}
+    alignItems="center"
+    
+    > 
+    
+    <Grid item xs={12}> <Question num="1"></Question> </Grid>
+    <Grid item xs={12}> <Question num="2"></Question> </Grid>
+    <Grid item xs={12}> <Question num="3"></Question> </Grid>
+  
+    </Grid>
 
 
     </>
