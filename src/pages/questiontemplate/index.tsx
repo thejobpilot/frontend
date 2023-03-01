9fca32c5c1c8b3b83a7e67363df2acfa04d7cd9c
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-02-28 21:45:05 -0500

    questiontemplate: save button

--- src/pages/questiontemplate/index.tsx
+++ src/pages/questiontemplate/index.tsx
@@ -5,18 +5,23 @@ import ResponsiveAppBar from "@/components/navBar";
 import Question from "@/components/interviewQuestion";
 import Grid from '@mui/material/Grid';
 import { margin } from '@mui/system';
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 const inter = Inter({subsets: ['latin']})
 
 export default function questiontemplate() {
   const divStyle = {
-  
     marginTop: '77px',
     justifyContent: 'center',
     alignItems: 'center'
- 
-    
   };
+
+  const buttonStyle = {
+    marginTop: '50px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '150px'
+  };
+
   return (
     <>
     <ResponsiveAppBar></ResponsiveAppBar>
@@ -32,9 +37,17 @@ export default function questiontemplate() {
       <Grid item xs={12}> <Question num="1"></Question> </Grid>
       <Grid item xs={12}> <Question num="2"></Question> </Grid>
       <Grid item xs={12}> <Question num="3"></Question> </Grid>
-    
+
       </Grid>
-      
+    </div>
+
+    <div style={buttonStyle}>
+    <Box textAlign='center'>
+      <Button variant="contained" size="large">
+            Save Template
+        </Button>
+
+    </Box>
     </div>
 
     </>
