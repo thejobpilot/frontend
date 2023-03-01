5fc6a5969db3dac0f8dbd02ef72f42390f0ac32d
Logan Nielson <lnielson@purdue.edu>
2023-03-01 17:25:40 -0500

    Working on the UI for Position List, Applicant List, and recruiter Dash

--- src/pages/recruiter/dash/index.tsx
+++ src/pages/recruiter/dash/index.tsx
@@ -4,6 +4,7 @@ import ResponsiveAppBar from "@/components/navBar";
 import Button from "@mui/material/Button";
 import React, { Component } from "react";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import AccountDetailsRecruiter from "@/components/accountDetailsRecruiter";
 
 export default function Profile() {
     const { user, error, isLoading } = useUser();
@@ -15,9 +16,14 @@ export default function Profile() {
         user && (
             <div style={{ backgroundColor: "#EFEFEF" }}>
                 <ResponsiveAppBar></ResponsiveAppBar>
-                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                    <Button style={{ backgroundColor: '#111E31', color: 'white', margin: '25px', padding: '25px 50px' , borderRadius: 16}} variant="contained">Manage Interviews</Button>
-                    <Button style={{ backgroundColor: '#111E31', color: 'white', margin: '25px', padding: '25px 50px' , borderRadius: 16}} variant="contained">Review Applicants</Button>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', height: '100vh' }}>
+                    <div style={{ backgroundColor: 'transparent', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                        <Button style={{ backgroundColor: '#111E31', color: 'white', margin: '25px', padding: '25px 50px', borderRadius: 16}} variant="contained">Manage Interviews</Button>
+                        <Button style={{ backgroundColor: '#111E31', color: 'white', margin: '25px', padding: '25px 50px', borderRadius: 16}} variant="contained">Review Applicants</Button>
+                    </div>
+                    <div style={{ flexGrow: 1 }}>
+                        <AccountDetailsRecruiter></AccountDetailsRecruiter>
+                    </div>
                 </div>
             </div>
         )
