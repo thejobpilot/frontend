3125a499750776f85a11ba38d8c7f3edffbb95d7
GeckoThePecko <bernie.gandin2@gmail.com>
2023-03-02 17:51:21 -0500

    Delete code

--- src/pages/api/auth/[...auth0].tsx
+++ src/pages/api/auth/[...auth0].tsx
@@ -5,27 +5,6 @@ import { AxiosError } from "axios";
 
 export default handleAuth({
   async login(req, res) {
-    let user: User = {
-      email: "john@google.com",
-      fullName: "john",
-      gpa: 0,
-      graduationDate: "2005-01-01",
-      resumeLink: "",
-      retakes: false,
-      userType: UserUserTypeEnum.Applicant,
-      username: "",
-    };
-    let usersApi = new UsersApi(
-      new Configuration({
-        basePath: "https://7kaz5avhmv.us-east-2.awsapprunner.com",
-      })
-    );
-    let axiosResponse = await usersApi
-      .createOneBaseUserControllerUser(user)
-      .catch((error: AxiosError) => {
-        console.log(error.response?.data);
-      })
-      .then((res) => console.log("worked"));
     await handleLogin(req, res, {
       returnTo: "/dash",
     });
