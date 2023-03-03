6d20888f80789e6fee49f3392a6621d974ed8b54
GeckoThePecko <bernie.gandin2@gmail.com>
2023-03-03 01:13:05 -0500

    Added set-interview api route and requestSetInterview

---
+++ src/components/db/requestSetInterview.tsx
@@ -0,0 +1,8 @@
+import {Interview, User} from "jobpilot-backend";
+
+export default async function requestSetInterview(interviewId: number, positonId: number, newInterview: Interview) {
+  await fetch(`/api/db/set-interview?id=${interviewId}&positionId=${positonId}`, {
+    body: JSON.stringify(newInterview),
+    method: "PUT",
+  });
+}
