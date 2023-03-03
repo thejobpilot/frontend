9cbb71b3e4d7e3b5fa81500c7236043ad02a593f
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-03-02 23:31:15 -0500

    Add some header img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
-import {Inter} from '@next/font/google'
-import { Button } from '@mui/material';
+import { Inter } from "@next/font/google";
+import { Box, Button } from "@mui/material";
 import ResponsiveAppBar from "@/components/navBar";
-import { display } from '@mui/system';
+import { display } from "@mui/system";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <ResponsiveAppBar></ResponsiveAppBar>
-      <div style={{display: "block"}}>
+      <div style={{ display: "block" }}>
+        <Box
+          component="img"
+          sx={{ mt: 30, mb: 0, mx: "40.5%", maxHeight: "300px"}}
+          src="/images/transpb.png"
+        />
         <Button
           type="submit"
           variant="contained"
-          sx={{ mt: 30, mb: 2, mx: "45%" }}
+          sx={{ mt: 3, mb: 2, mx: "45%" }}
           href="/login"
         >
           Get started
