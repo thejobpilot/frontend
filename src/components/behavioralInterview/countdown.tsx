0f5efbd4d4e198a324289bc5c22ec05a50827ba4
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-03-27 22:45:05 -0400

    fixed countdown and line

--- src/components/behavioralInterview/countdown.tsx
+++ src/components/behavioralInterview/countdown.tsx
@@ -4,15 +4,16 @@ import Typography from '@mui/material/Typography';
 
 interface CountdownProps {
   seconds: number;
+  onCountdownUpdate: (progress: number) => void;
 }
 
-const Countdown: React.FC<CountdownProps> = ({ seconds }) => {
+const Countdown: React.FC<CountdownProps> = ({ seconds, onCountdownUpdate }) => {
   const [timeLeft, setTimeLeft] = useState(seconds);
 
   useEffect(() => {
     const timer = setInterval(() => {
       setTimeLeft((prevTimeLeft) => {
-        if (prevTimeLeft === 0) {
+        if (prevTimeLeft <= 1) {
           clearInterval(timer);
           return 0;
         }
@@ -25,9 +26,19 @@ const Countdown: React.FC<CountdownProps> = ({ seconds }) => {
     };
   }, []);
 
+  useEffect(() => {
+    onCountdownUpdate((timeLeft / seconds) * 100);
+  }, [timeLeft, seconds, onCountdownUpdate]);
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  };
+
   return (
-    <Typography variant="h6">
-      Time left: {Math.floor(timeLeft / 60)}:{timeLeft % 60}
+    <Typography variant="h6" component="div">
+      Time left: {formatTime(timeLeft)}
     </Typography>
   );
 };
