50ce390a91e9c5c1f0aef08593e3aafad3417019
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-03-30 01:20:47 -0400

    fixFolders plus run api stuff

--- src/pages/applicant/behavioral/dash/index.tsx
+++ src/pages/applicant/behavioral/dash/index.tsx
@@ -3,9 +3,6 @@ import NavBar from "@/components/navBar";
 import SignIn from "@/components/signIn";
 import SignUp from "@/components/signUp";
 import ResponsiveAppBar from "@/components/navBar";
-import Countdown from "@/components/interviewComponents/countdown";
-import InterviewPage from "@/components/interviewComponents/interviewPage";
-import Question from "@/components/interviewComponents/questions";
 import InterviewDash from "@/components/behavioralInterview/interviewDash";
 
 const inter = Inter({subsets: ['latin']})
@@ -14,7 +11,8 @@ export default function Behavioral() {
     return (
         <>
             <ResponsiveAppBar></ResponsiveAppBar>
-            <InterviewPage></InterviewPage>
+            <InterviewDash></InterviewDash>
+     
         </>
     )
 }
