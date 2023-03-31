3be25d9049e4e58e551c7f6a27a726663dd8926f
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-03-31 09:50:06 -0400

    Time and date

--- src/pages/applicant/summary/[id].tsx
+++ src/pages/applicant/summary/[id].tsx
@@ -23,7 +23,13 @@ import questions from "@/components/interviewComponents/questions";
 import ResponseCard from "./responseCard";
 import ResponsiveAppBar from "@/components/navBar";
 
-function userHasInterviewID(user: any, id: any, updater: any, updaterq: any, interview: any) {
+function userHasInterviewID(
+  user: any,
+  id: any,
+  updater: any,
+  updaterq: any,
+  interview: any
+) {
   if (!user) {
     updater(null);
     return;
@@ -31,20 +37,19 @@ function userHasInterviewID(user: any, id: any, updater: any, updaterq: any, int
   id! = parseInt(id);
   for (const i in user.interviews) {
     if (user.interviews[i].id === id) {
-
       let response = user.interviews[i].responses.find(
         (response: any) => response.applicantEmail === user?.email
       );
       let questions = [];
       response.textAnswers.forEach((answer: any) => {
         let question = user.interviews[i].questions.find(
-          (q  : any) => q.id === answer.questionId
+          (q: any) => q.id === answer.questionId
         );
         questions.push({
           question: question,
           answer: answer,
         });
-      })
+      });
 
       updater(response);
       updaterq(questions);
@@ -82,21 +87,23 @@ export function Summary() {
       setSelectedQuestion(question);
     }
   };
-  
+
   function fetchDate(interview: any) {
     let path = `end_time_${interview.id}-${interview.interviewLength}`;
-     const savedEndTime =
+    const savedEndTime =
       typeof window !== "undefined" && localStorage.getItem(path);
-      if (savedEndTime) return savedEndTime 
-      else return "<corrupted>"
-
+    if (savedEndTime) return savedEndTime;
+    else return "<corrupted>";
   }
-  const options = {
-    weekday: "long",
-    year: "numeric",
+  const options: Intl.DateTimeFormatOptions = {
     month: "long",
-    day: "numeric",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    timeZone: "EST",
   };
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
   if (isLoadingDB) return <div>Loading...</div>;
@@ -111,12 +118,26 @@ export function Summary() {
         <ResponsiveAppBar />
         <Container maxWidth="md" sx={{ mt: 15 }}>
           <Box sx={{ mt: 4 }}>
-            <Typography variant="h4" mb={2}>
-              Response Summary
-              <Typography variant="h6" mb={2}>
-                Completion date:{" "}
+            <Typography variant="h4" mb={2} fontSize="30px">
+              <Typography
+                variant="h4"
+                mb={2}
+                sx={{fontWeight: "bold"}}
+              >
+                {interview.name}
+              </Typography>
+              Summary of Responses
+              <Typography
+                variant="h6"
+                mb={2}
+                sx={{ fontWeight: "normal", opacity: "50%", mt: 1 }}
+              >
+                Completed:{" "}
                 {interview &&
-                  new Date(fetchDate(interview)).toLocaleDateString(undefined, options)}
+                  new Date(fetchDate(interview)).toLocaleDateString(
+                    undefined,
+                    options
+                  )}
               </Typography>
             </Typography>
             <div>
