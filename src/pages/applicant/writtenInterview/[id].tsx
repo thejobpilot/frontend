81617b4e3a5178cf199561c9c898a5f7af536a79
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-03-31 02:46:13 -0400

    Add reset timer

--- src/pages/applicant/writtenInterview/[id].tsx
+++ src/pages/applicant/writtenInterview/[id].tsx
@@ -1,36 +1,39 @@
 import ResponsiveAppBar from "@/components/navBar";
 import InterviewPage from "@/components/interviewComponents/interviewPage";
-import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import {useUser} from "@auth0/nextjs-auth0/client";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import useUserDB from "@/components/db/useUserDB";
+import { withTitle } from "@/components/utils";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-export default function Written(props: any) {
-    const router = useRouter();
-    const {id} = router.query;
-    const {user, error, isLoading} = useUser();
-    const [interview, setInterview] = useState<any>(null);
+export function Written(props: any) {
+  const router = useRouter();
+  const { id } = router.query;
+  const { user, error, isLoading } = useUser();
+  const [interview, setInterview] = useState<any>(null);
 
-    const {
-        data,
-        isLoading: isLoadingDB,
-        isError,
-    } = useUserDB(user ? user.email! : "");
-    console.log(id)
-    useEffect(() => {
-        if (data && data.interviews) {
-            setInterview(data.interviews.find(interview => interview.id == id));
-        }
-    }, [data]);
+  const {
+    data,
+    isLoading: isLoadingDB,
+    isError,
+  } = useUserDB(user ? user.email! : "");
 
-    return (
-        <>
-            <ResponsiveAppBar></ResponsiveAppBar>
-            <InterviewPage
-                user={data}
-                interview={interview}
-            ></InterviewPage>
-        </>
-    );
+  useEffect(() => {
+    if (data && data.interviews) {
+      setInterview(
+        data.interviews.find((interview: any) => interview.id == id)
+      );
+    }
+  }, [data]);
+
+  return (
+    <>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <InterviewPage user={data} interview={interview}></InterviewPage>
+    </>
+  );
 }
 
+export const getServerSideProps = withPageAuthRequired();
+export default withTitle("Written Interview")(Written);
