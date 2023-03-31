d257c42c50e5784dfc9438d060a687cb429ab6ee
GeckoThePecko <bernie.gandin2@gmail.com>
2023-03-31 00:25:30 -0400

    Fix set-interview

--- src/pages/api/db/set-interview.tsx
+++ src/pages/api/db/set-interview.tsx
@@ -1,29 +1,34 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import {NextApiRequest, NextApiResponse} from "next";
 import api from "@/utils/utils";
-import { ApiErrorResponse, ApiOkResponse } from "apisauce";
+import {ApiErrorResponse, ApiOkResponse} from "apisauce";
 
 export default async function updateUser(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  const id: number = parseInt(req.query.id as string);
-  const positionId: number = parseInt(req.query.positionId as string);
-  // const interview = JSON.parse(req.body);
-  // const formattedInterview = {
-  //   name: interview.name,
-  //   prepTime: parseInt(interview.prepTime),
-  //   retakes: parseInt(interview.retakes),
-  // };
+    const id: number = parseInt(req.query.id as string);
+    const positionId: number = parseInt(req.query.positionId as string);
+    // const interview = JSON.parse(req.body);
+    let body = JSON.parse(req.body);
+    const formattedInterview = {
+        name: body.name,
+        prepTime: parseInt(body.prepTime),
+        retakes: parseInt(body.retakes),
+        videoURL: body.videoURL,
+        interviewType: body.interviewType,
+        companyName: body.companyName,
+    };
+    console.log(formattedInterview);
 
-  const response: ApiErrorResponse<unknown> | ApiOkResponse<unknown> =
-    await api.patch(`/position/${positionId}/interview/${id}`, req.body);
+    const response: ApiErrorResponse<unknown> | ApiOkResponse<unknown> =
+        await api.patch(`/position/${positionId}/interview/${id}`, formattedInterview);
 
-  if (response.ok) {
-    res.status(200).json(response.data);
-  } else {
-    console.log("fail", response?.data);
-    res.status(500).send(`Internal Server Error: ${response?.data}`);
-  }
+    if (response.ok) {
+        res.status(200).json(response.data);
+    } else {
+        console.log("fail", response?.data);
+        res.status(500).send(`Internal Server Error: ${response?.data}`);
+    }
 }
 
 // async function updateUser(
