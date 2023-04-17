47246f9ee6bedef0f818b6834447057707ae4052
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2023-04-16 21:21:19 -0400

    added microphone perms

--- src/components/interviewComponents/permissions.tsx
+++ src/components/interviewComponents/permissions.tsx
@@ -9,7 +9,7 @@ const Permissions: React.FC = () => {
 
   const handleStartRecording = async () => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
 
       if (videoRef.current) {
         videoRef.current.srcObject = stream;
