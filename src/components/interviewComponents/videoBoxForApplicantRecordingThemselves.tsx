c1d9bd7381aa408c3f107b2cd438eddb30bebd4b
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2023-04-20 18:15:37 -0400

    Almost fix recording

--- src/components/interviewComponents/videoBoxForApplicantRecordingThemselves.tsx
+++ src/components/interviewComponents/videoBoxForApplicantRecordingThemselves.tsx
@@ -1,134 +1,166 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import React, { useRef, useState, useEffect } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import {
+    PlayCircle,
+  RadioButtonChecked,
+  RestartAlt,
+  StopCircle,
+} from "@mui/icons-material";
 
 interface VideoRecorderProps {
-    onRecordingComplete: (blob: Blob) => void;
-    reset: boolean;
-    currentQuestionIndex: number;
+  onRecordingComplete: (blob: Blob) => void;
+  reset: boolean;
+  currentQuestionIndex: number;
 }
 
-const VideoRecorder: React.FC<VideoRecorderProps> = ({ onRecordingComplete, reset, currentQuestionIndex }) => {
-    const videoRef = useRef<HTMLVideoElement>(null);
-    const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
-    const [status, setStatus] = useState<'idle' | 'recording' | 'stopped'>('idle');
-    const [attempts, setAttempts] = useState(0);
-    const maxAttempts = 3;
+const VideoRecorder: React.FC<VideoRecorderProps> = ({
+  onRecordingComplete,
+  reset,
+  currentQuestionIndex,
+}) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
+  const [attempts, setAttempts] = useState(0);
+  const maxAttempts = 3;
 
-    useState(currentQuestionIndex);
+  const initMediaRecorder = async () => {
+    if (videoRef.current && !mediaRecorder) {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      videoRef.current.srcObject = stream;
 
-    useEffect(() => {
-        const resetVideo = async () => {
-            if (videoRef.current) {
-                setAttempts(0);
-                videoRef.current.src = '';
-                videoRef.current.load();
-                if (mediaRecorder && status !== 'recording') {
-                    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-                    videoRef.current.srcObject = stream;
-                }
-            }
-        };
+      const recorder = new MediaRecorder(stream);
+      setMediaRecorder(recorder);
+    }
+  };
 
-        resetVideo();
-    }, [currentQuestionIndex]);
-
-
-    useEffect(() => {
-        const initMediaRecorder = async () => {
-            if (videoRef.current && !mediaRecorder) {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-                videoRef.current.srcObject = stream;
-
-                const recorder = new MediaRecorder(stream);
-                setMediaRecorder(recorder);
-            }
-        };
-
-        initMediaRecorder();
-    }, [mediaRecorder, currentQuestionIndex]);
-
-    useEffect(() => {
-        if (mediaRecorder) {
-            const chunks: Blob[] = [];
-
-            mediaRecorder.ondataavailable = (event) => {
-                if (event.data.size > 0) {
-                    chunks.push(event.data);
-                }
-            };
-
-            mediaRecorder.onstart = (event) => {
-                chunks.length = 0;
-            }
-
-            mediaRecorder.onstop = () => {
-                const blob = new Blob(chunks, { type: 'video/webm' });
-                onRecordingComplete(blob);
-                console.log("Updating")
-                if (videoRef.current) {
-                    videoRef.current.srcObject = null;
-                    videoRef.current.src = URL.createObjectURL(blob);
-                }
-            };
+  useState(currentQuestionIndex);
+  useEffect(() => {
+    const resetVideo = async () => {
+      if (videoRef.current) {
+        setAttempts(0);
+        videoRef.current.src = "";
+        videoRef.current.load();
+        if (mediaRecorder && mediaRecorder.state !== "recording") {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+          });
+          videoRef.current.srcObject = stream;
         }
-    }, [mediaRecorder, onRecordingComplete, currentQuestionIndex]);
-
-    useEffect(() => {
-        if (reset) {
-            setStatus('idle');
-            setAttempts(0);
-            if (videoRef.current) {
-                videoRef.current.src = '';
-                videoRef.current.load();
-            }
-        }
-    }, [reset]);
-
-    const handleStartRecording = async () => {
-        if (mediaRecorder && status !== 'recording' && attempts < maxAttempts) {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-            if(videoRef.current) {
-                videoRef.current.srcObject = stream;
-            }
-            setStatus('recording');
-            setAttempts(attempts + 1);
-            mediaRecorder.start();
-        }
+      }
     };
 
-    const handleStopRecording = () => {
-        if (mediaRecorder && status === 'recording') {
-            mediaRecorder.stop();
-            setStatus('stopped');
-        }
-    };
+    resetVideo();
+  }, [currentQuestionIndex]);
 
-    return (
-        <Box textAlign="center">
-            <video ref={videoRef} width="50%" autoPlay muted controls />
-            <div>Status: {status}</div>
-            <Typography variant="body1">
-                Attempt {attempts} of {maxAttempts}
-            </Typography>
-            <Box mt={2}>
-                <Button
-                    variant="contained"
-                    onClick={handleStartRecording}
-                    disabled={status === 'recording' || attempts >= maxAttempts}
-                >
-                    Record
-                </Button>
-                <Button
-                    variant="contained"
-                    onClick={handleStopRecording}
-                    disabled={status !== 'recording'}
-                    style={{ marginLeft: '1rem' }}
-                >
-                    Stop
-                </Button>
-            </Box>
-        </Box>
-    );
+  useEffect(() => {
+    initMediaRecorder();
+    return () => {
+      console.log("media recorder state", mediaRecorder?.state);
+    };
+  }, [mediaRecorder, currentQuestionIndex]);
+
+  useEffect(() => {
+    if (mediaRecorder) {
+      const chunks: Blob[] = [];
+
+      mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          chunks.push(event.data);
+        }
+      };
+
+      mediaRecorder.onstart = (event) => {
+        chunks.length = 0;
+      };
+
+      mediaRecorder.onstop = () => {
+        const blob = new Blob(chunks, { type: "video/webm" });
+        onRecordingComplete(blob);
+        console.log("Updating");
+        if (videoRef.current) {
+          videoRef.current.srcObject = null;
+          videoRef.current.src = URL.createObjectURL(blob);
+        }
+      };
+    }
+  }, [mediaRecorder, onRecordingComplete, currentQuestionIndex]);
+
+  useEffect(() => {
+    if (reset) {
+      setAttempts(0);
+      if (videoRef.current) {
+        videoRef.current.src = "";
+        videoRef.current.load();
+      }
+    }
+  }, [reset]);
+
+  const handleStartRecording = async () => {
+    if (
+      mediaRecorder &&
+      mediaRecorder.state !== "recording" &&
+      attempts < maxAttempts
+    ) {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+      setAttempts(attempts + 1);
+      mediaRecorder.start();
+    }
+  };
+
+  const handleStopRecording = () => {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+    }
+  };
+
+  return (
+    <Box textAlign="center">
+      <video
+        ref={videoRef}
+        width="50%"
+        autoPlay
+        muted
+        controls={mediaRecorder?.state === "inactive"}
+      />
+      <div>Status: {mediaRecorder?.state}</div>
+      <Typography variant="body1">
+        Attempt {attempts} of {maxAttempts}
+      </Typography>
+      <Box mt={2}>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={handleStartRecording}
+          disabled={
+            mediaRecorder?.state === "recording" || attempts >= maxAttempts
+          }
+          endIcon={<RadioButtonChecked />}
+        >
+          Record
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={handleStopRecording}
+          disabled={mediaRecorder?.state !== "recording"}
+          style={{ marginLeft: "1rem" }}
+          endIcon={<StopCircle />}
+        >
+          Stop
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default VideoRecorder;
